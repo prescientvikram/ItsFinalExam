f4ff1fd56b9ac861c8db90efa1da54071ba09541
vivekdhake74 <vivekdhake74@gmail.com>
2023-08-22 14:50:52 +0530

    first commit17

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,11 @@
 import json
 import os
-import webbrowser
+#import webbrowser
 import urllib.request
 ##import requests
-import  lambda_function
+#import  lambda_function
 
-def lambda_handler(event, context):
+def lambda_handler():
 
  payload = {
       "subnet_id": "aws_subnet.private_subnet.id",
@@ -13,13 +13,15 @@ def lambda_handler(event, context):
       "email": "vikramwaghmare7995@gmail.com"
     }
 
+headers = {'content-type': {'X-Siemens-Auth': 'test'}}
+
 data = json.loads(payload)
 
 # The API endpoint to communicate with
 url_post = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/"
 
 # A POST request to tthe API
-post_response = urllib.request.post(url_post, json=data, headers={'content-type': {'X-Siemens-Auth': 'test'}})
+post_response = urllib.request.post(url_post, json=data,headers=headers)
 
 # Print the response
 post_response_json = post_response.json()
