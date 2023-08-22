7d05419954e5b14c1f9e172c355d607475fd08a8
prescientvikram <125640833+prescientvikram@users.noreply.github.com>
2023-08-22 12:13:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "routing_table" {
     route {
 
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.nat.id
+        nat_gateway_id = aws_nat_gateway.nat.id
     }
 
 
