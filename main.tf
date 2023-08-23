68e877448407ade110ddb70cab33c27ac8614e14
prescientvikram <125640833+prescientvikram@users.noreply.github.com>
2023-08-23 10:20:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,13 +28,14 @@ resource "aws_route_table" "routing_table" {
 
 ## aws lambda fuction  to invoke an remote api 
 
-resource "aws_lambda_function" "aws_lambda7" {
-  function_name    = "aws_lambda7"
+resource "aws_lambda_function" "aws_lambda1" {
+  function_name    = "aws_lambda1"
   filename         = "lambda_function.zip"
   source_code_hash = filebase64sha256("lambda_function.zip")
   handler          = "lambda_function.lambda_handler"
   role             =  data.aws_iam_role.lambda.arn
   runtime          = "python3.7"
+  timeout          =  30
   
 
   vpc_config {
