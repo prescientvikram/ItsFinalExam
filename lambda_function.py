0498c7971990dcaabd5d6d079a0dd73f655422f9
prescientvikram <125640833+prescientvikram@users.noreply.github.com>
2023-08-23 11:48:44 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -18,28 +18,17 @@ def lambda_handler(event, context):
         "email": "vikramwaghmare7995@gmail.com"
     }
 
- 
-
     # Convert the payload to JSON
     json_payload = json.dumps(payload)
 
- 
 
     headers ={
         "Content-Type": "application/json",
         "X-Siemens-Auth": "test"
     }
 
-    print("lambda_handler 4.")
-
- 
-
     # Make a POST request to the remote API endpoint
     url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
     response = requests.post(url, headers=headers, data=json_payload)
 
- 
-
- 
-
-    print("lambda_handler 5.",response)
+   
